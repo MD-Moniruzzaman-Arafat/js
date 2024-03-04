60e679e4111b80fe6e55cce3585c2213c41d4e53
md-moniruzzaman-arafat <arafatmonir091@gmail.com>
2024-03-04 16:06:25 +0600

    valid variable name

---
+++ variable.js
@@ -0,0 +1,11 @@
+var name = "arafat";
+var number = 07;
+var isRunning = true;
+var _animal = "cow";
+var $isFavorite = false;
+
+console.log(name);
+console.log(number);
+console.log(isRunning);
+console.log(_animal);
+console.log($isFavorite);
