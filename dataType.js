056ba4def5786fb11330b9e96d0d13b7cb8c662b
md-moniruzzaman-arafat <arafatmonir091@gmail.com>
2024-03-07 17:25:12 +0600

    explore primitive data type

---
+++ dataType.js
@@ -0,0 +1,46 @@
+/* 
+Data Type :
+1. Primitive data type
+1.1 Number
+1.2 String
+1.3 Boolean
+1.4 Undefine
+1.5 Null
+1.6 Bigint
+1.7 Symbol
+2. Object data type
+2.1 An object
+2.2 An array
+2.3 A date
+*/
+
+/* 
+---------------------------------
+      INTERVIEW QUESTION
+---------------------------------
+1. what is the difference between null and undefine in javaScript?
+2. what is the purpose of typeof operator in javaScript?
+3. what is the result typeof null in javaScript?
+4. what are primitive dataType in javaScript?
+5. convert a string to a number?
+6. convert a number to a string?
+7. explain the concept of truthy and falsy value in javaScript?
+8. to check if a non-empty string is truthy or falsy in javaScript, we can directly use if statement. 
+*/
+
+// Primitive data type
+
+var number = 10;
+console.log(typeof number);
+
+var myName = "arafat";
+console.log(typeof myName);
+
+var isRunning = true;
+console.log(typeof isRunning);
+
+var booksName;
+console.log(typeof booksName);
+
+var isNumber = null;
+console.log(typeof isNumber);
