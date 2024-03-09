a4cd33a18ad66924a6b1a485cdd5c95a438ae9dc
md-moniruzzaman-arafat <arafatmonir091@gmail.com>
2024-03-09 23:01:54 +0600

    javaScript values & variable

--- variable.js
+++ variable.js
@@ -1,11 +1,34 @@
-var name = "arafat";
-var number = 07;
-var isRunning = true;
-var _animal = "cow";
-var $isFavorite = false;
+/*
+==========================================
+      javaScript values & variable
+==========================================
 
-console.log(name);
-console.log(number);
-console.log(isRunning);
-console.log(_animal);
-console.log($isFavorite);
+** In javaScript values and variable are fundamental concepts that form the basis of programming.
+** values: A value is a piece of information that a program can work with. it can be a number, text, true/false,or more complex data.
+** variable: A variable is a container that holds a value. it has a name and can be used to store and manipulate data in a program.
+
+
+
+
+
+
+ */
+
+// valid: its start with character
+/* var my_firstName = "john";
+console.log(my_firstName); */
+
+// valid: its start with underscore
+/* var _myLastName$ = "due";
+console.log(_myLastName$); */
+
+// invalid: its start with number
+/* var 123myAge = 24
+console.log(123myAge) */
+
+// valid: its start with dollar
+/* var $cityName = "dhaka";
+console.log($cityName); */
+
+var my@Email = "arafat@gmail.com"
+console.log(my@Email)
