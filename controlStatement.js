b2411f9a43b6e6d03add0d061fc42298b19b25d6
md-moniruzzaman-arafat <arafatmonir091@gmail.com>
2024-03-10 17:47:09 +0600

    javascript control statement

---
+++ controlStatement.js
@@ -0,0 +1,60 @@
+/* 
+==========================================
+      javascript control statement
+==========================================
+1.if
+2.if...else
+3.switch 
+*/
+
+// if
+/* var num = 5;
+
+if (num > 0) {
+  console.log("The number is positive.");
+} */
+
+// if else
+/* var num = -10;
+
+if (num > 0) {
+  console.log("The number is positive.");
+} else {
+  console.log("The number is negative");
+} */
+
+/* var time = 12;
+
+if (time < 10) {
+  console.log("Good morning");
+} else if (time < 20) {
+  console.log("Good day");
+} else {
+  console.log("Good evening");
+} */
+
+// switch
+/* var day;
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+}
+console.log(day); */
