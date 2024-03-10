d05978a1c0ad19669d13c1dab4063887862b3cc8
md-moniruzzaman-arafat <arafatmonir091@gmail.com>
2024-03-10 13:08:27 +0600

    explore operator in javaScript

---
+++ operator.js
@@ -0,0 +1,77 @@
+/* 
+============================================
+      expression & operator
+============================================
+** types of operator
+1.assignment operator
+2.arithmetic operator
+3.comparison operator
+4.logical operator
+5.string operator
+6.unary operator 
+7.ternary operator
+8.type operator
+*/
+
+// assignment operator:
+/* var myName = "arafat";
+var myNumber = 7;
+var price = 15; */
+
+// arithmetic operator
+/* console.log(price + 10);
+console.log(price - 10);
+console.log(price * 10);
+console.log(price / 10);
+console.log(price % 10); */
+
+// comparison operator
+/* console.log(price > 10);
+console.log(price < 10);
+console.log(price >= 15);
+console.log(price <= 9);
+console.log(price == 15);
+console.log(price === "15");
+console.log(price != 15);
+console.log(price !== 50); */
+
+// logical operator
+// && || !
+/* let hour = 12;
+let minute = 30;
+
+if (hour == 12 && minute == 30) {
+  console.log("The time is 12:30");
+} */
+
+/* let hour = 9;
+
+if (hour < 10 || hour > 18) {
+  console.log("The office is closed.");
+} */
+
+/* console.log(!true); */
+
+// ternary operator
+/* var age = 18;
+age >= 18 ? console.log("eligible") : console.log("not eligible"); */
+
+/* 
+====================================
+      interview question
+====================================
+*/
+
+/* var result = 0.1 + 0.2;
+console.log(result.toFixed(1)); */
+
+// write a program that determines if a person is eligible to drive based on their age being greater then or equal to 18 and having a valid drivers license?
+/* var age = 18;
+var license = true;
+
+if (age >= 18 && license) {
+  console.log("eligible to drive");
+} */
+
+/* console.log(2 < 12 < 5);
+console.log("20" + 10 + 10); */
