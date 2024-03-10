e57833497a553659e898f07cf89e730f41daf8da
md-moniruzzaman-arafat <arafatmonir091@gmail.com>
2024-03-10 11:29:15 +0600

    explore concate in javaScript

---
+++ concat.js
@@ -0,0 +1,25 @@
+/* 
+==================================================
+      javaScript concat
+==================================================
+ */
+
+/* console.log(10 + "hello");
+console.log(10 + "10"); */
+
+/* 
+==================================================
+      interview question
+==================================================
+*/
+/* console.log(10 + "20");
+console.log(9 - "5");
+console.log("java" + "Script");
+console.log(" " + " ");
+console.log(" " + 0);
+console.log("john" - "due");
+console.log(true + true);
+console.log(true + false);
+console.log(false + true);
+console.log(false - true);
+console.log(true - false); */
