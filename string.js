2d545bb1adcbe4a4f81c724e08b50472e9f291a0
md-moniruzzaman-arafat <arafatmonir091@gmail.com>
2024-03-14 23:22:38 +0600

    javascript string

---
+++ string.js
@@ -0,0 +1,52 @@
+/* 
+=================================
+      javaScript string
+=================================
+** what we will cover
+1. string & it's properties
+2. string search method
+3. extracting string characters
+4. escape character
+5. extracting string part
+6. replacing string content
+7 others useful methods
+ */
+
+// string properties
+// length
+/* const demo = "hello world";
+console.log(demo.length);
+ */
+
+// string search method
+// indexof
+/* 
+const demo = "hello world";
+console.log(demo.indexOf("world"));
+ */
+
+// lastindexof
+/* const demo = "hello world hello";
+console.log(demo.lastIndexOf("hello")); */
+
+// search method
+/* const texts = "hello world bangladesh";
+console.log(texts.search("hello")); */
+
+// includes
+/* const texts = "hello world bangladesh";
+console.log(texts.includes("hellsso")); */
+
+// start with method
+/* const texts = "hello world bangladesh";
+console.log(texts.startsWith("world"));
+ */
+
+// end with method
+/* const texts = "hello world bangladesh";
+console.log(texts.endsWith("s"));
+console.log(texts.endsWith("h")); */
+
+// slice method
+/* const texts = "hello world bangladesh";
+console.log(texts.slice(5, 11)); */
